b8463bb01d381472a03b0ad0999b6ba954cff169
Rafael Joao <rafael.joaods@gmail.com>
2022-11-16 17:49:37 -0300

    first commit

---
+++ Desafio Aula 2/main.js
@@ -0,0 +1,4 @@
+//Uma referência que armazene uma lista com todas as teclas e imprima o valor dela na aba console.
+
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+console.log(listaDeTeclas);
---
+++ Desafio primeira aula/main.js
@@ -0,0 +1,5 @@
+//Escreva o código em JavaScript capaz de selecionar o elemento HTML input do tipo telefone:
+
+
+
+document.querySelectorAll('input')[12]
